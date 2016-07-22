921954c893cccae84f0d9462a1d2922f044f57cc
jinkimdata <jinkimdata@gmail.com>
2016-07-22 11:47:01 -0400

    added social btns

--- dist/js/app.js
+++ dist/js/app.js
@@ -2951,6 +2951,18 @@
 
 var ravensBest = {
     init: function() {
+        window.onload = function() {
+            var hash = window.location.hash.substring(1);
+            if (hash) {
+                $("html, body").animate({
+                    scrollTop: $("#anchor" + hash).offset().top
+                }, 500);
+            }
+        };
+        $(".splash").one("click touchend", function() {
+            $(this).fadeOut();
+            return false;
+        });
         $(".downButton").on("click touchend", function() {
             var nextSlide = Number($(this).attr("data-slideNum")) + 1;
             $("html, body").animate({
@@ -2965,8 +2977,24 @@ var ravensBest = {
             }, 500);
             return false;
         });
-        $(".splash").one("click touchend", function() {
-            $(this).fadeOut();
+        ravensBest.share();
+    },
+    share: function() {
+        $(".icon-twitter").on("click", function() {
+            var tweet = $(this).attr("data-shareText") + " - remember this play? Vote for the top Ravens moments at ";
+            var url = "http://data.baltimoresun.com/sports/ravens-best-plays/%23" + $(this).attr("data-slideNum");
+            var twitter_url = "https://twitter.com/intent/tweet?text=" + tweet + "&url=" + url + "&tw_p=tweetbutton";
+            window.open(twitter_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
+            return false;
+        });
+        $(".icon-facebook").on("click", function() {
+            var picture = "http://data.baltimoresun.com/sports/ravens-best-plays/images/thumbnail.png";
+            var title = "Readers Vote: Ravens' Best Plays";
+            var description = "We chose 10 of the top plays in Ravens history. Relive them, then scroll to the bottom to rank them based on your favorites.";
+            var url = "http://data.baltimoresun.com/sports/ravens-best-plays/";
+            var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link=" + url + "&picture=" + picture + "&name=" + title + "&description=" + description + "&redirect_uri=http://www.facebook.com";
+            window.open(facebook_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
+            return false;
         });
     }
 };
--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,6 +1,17 @@
 var ravensBest = {
 	init: function(){
-		//ravensBest.share();
+		window.onload = function() {
+			var hash = window.location.hash.substring(1);
+			if (hash) {
+				$('html, body').animate({
+					scrollTop: $('#anchor'+hash).offset().top
+				}, 500);
+			};
+		};
+		$('.splash').one('click touchend', function(){
+			$(this).fadeOut();
+			return false;
+		});
 		$('.downButton').on('click touchend', function(){
 			var nextSlide = Number($(this).attr('data-slideNum')) + 1;
 			$('html, body').animate({
@@ -15,29 +26,28 @@ var ravensBest = {
 			}, 500);
 			return false;
 		});
-		$('.splash').one('click touchend', function(){
-			// $('.splashImg').fadeOut('slow', function(){
-			// 	$('.splash').fadeOut('slow');
-			// });
-			$(this).fadeOut();
-		});
+		ravensBest.share();
 	},
-	// share: function(){
-	// 	$(".icon-twitter").on("click", function(){
-	// 		var tweet = ""; //Tweet text
-	// 		var url = ""; //Interactive URL
-	// 		var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
-	// 		window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
-	// 	});
-	// 	$(".icon-facebook").on("click", function(){
-	// 		var picture = ""; //Picture URL
-	// 		var title = ""; //Post title
-	// 		var description = ""; //Post description
-	// 		var url = ""; //Interactive URL
-	//     	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
-	// 		window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
-	// 	});
-	// }
+	share: function(){
+		$(".icon-twitter").on("click", function(){
+			var tweet = $(this).attr('data-shareText') + 
+				" - remember this play? Vote for the top Ravens moments at "; //Tweet text
+			var url = "http://data.baltimoresun.com/sports/ravens-best-plays/%23" + 
+				$(this).attr('data-slideNum'); //Interactive URL
+			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
+			window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); 
+			return false;
+		});
+		$(".icon-facebook").on("click", function(){
+			var picture = "http://data.baltimoresun.com/sports/ravens-best-plays/images/thumbnail.png"; //Picture URL
+			var title = "Readers Vote: Ravens' Best Plays"; //Post title
+			var description = "We chose 10 of the top plays in Ravens history. Relive them, then scroll to the bottom to rank them based on your favorites."; //Post description
+			var url = "http://data.baltimoresun.com/sports/ravens-best-plays/"; //Interactive URL
+	    	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";
+			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); 
+			return false;
+		});
+	}
 }
 $(document).ready(function(){
 	ravensBest.init();
